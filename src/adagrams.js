2a90f241272dad041861f532c1d619294a53d5b6
Malik Elmessiry <malik.messiry@gmail.com>
2025-05-22 12:55:00 +0200

    add drawLetters and helper function

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,25 @@
+const letterPool = {
+  'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 
+  'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 
+  'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1
+}
+
+const getAllLetters = () => {
+  let allLetters = [];
+  for (const letter in letterPool) {
+    const freq = letterPool[letter];
+    allLetters.push(...letter.repeat(freq))
+  } return allLetters;
+}
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  let allLetters = getAllLetters();
+  let hand = [];
+  for (let i = 0; i < 10; i++) {
+    let randIndex = Math.floor(Math.random() * allLetters.length);
+    let letter = allLetters.splice(randIndex, 1)[0];
+    hand.push(letter);
+  } return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
@@ -13,3 +33,4 @@ export const scoreWord = (word) => {
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
 };
+
